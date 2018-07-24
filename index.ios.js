9d6ff25f36a5ba48d03f9c3bb12be8f90eb56764
angelossdias <D14sAng>
2018-07-24 17:08:06 -0300

    usando a propriedade flex

--- index.ios.js
+++ index.ios.js
@@ -2,43 +2,54 @@ import React from 'react';
 import {Text, AppRegistry, View} from 'react-native';
 
 const Estilos = {
-  conteudo1: {
-    fontSize: 40,
-    backgroundColor: '#08509B',
+  principal: {
+    backgroundColor: 'cornflowerblue',
+    flex: 1 // vai preencher o espaço na proporção que é passada, no caso 1:1
+    // height: 300,
+    // justifyContent: 'space-around',
+    // alignItems: 'stretch',
+    // flexDirection: 'row'
+  },
+
+  topo: {
+    // fontSize: 40,
+    backgroundColor: 'brown',
+    flex: 2 // 2:7 porque divide espaço com os outros elementos, a partir do valor passado para cada, dentro do principal
     // paddingTop: 15,
     // color: '#fff',
     // fontStyle: 'italic',
     // fontWeight: 'bold' // ou valores entre 100 e 900
     // textAlign: 'center'
     // textDecorationLine: 'underline line-through'
-    height: 60,
-    width: 60
+    // height: 60,
+    // width: 60
   },
 
-  conteudo2: {
-    fontSize: 40,
-    backgroundColor: '#2A48FA',
-    height: 60,
-    width: 60
+  conteudo: {
+    // fontSize: 40,
+    backgroundColor: 'yellowgreen',
+    flex: 4
+    // height: 60,
+    // width: 60
   },
 
-  principal: {
-    backgroundColor: 'skyblue',
-    height: 300,
-    justifyContent: 'space-around',
-    alignItems: 'stretch',
-    flexDirection: 'row'
+  rodape: {
+    // fontSize: 40,
+    backgroundColor: 'orangered',
+    flex: 1
+    // height: 60,
+    // width: 60
   }
 };
 
 const App = () => {
-  const {conteudo1, principal} = Estilos;
+  const {topo, principal, conteudo, rodape} = Estilos;
 
   return (
     <View style={principal}>
-      <Text style={conteudo1}>A</Text>
-      <Text style={conteudo2}>B</Text>
-      <Text style={conteudo1}>C</Text>
+      <Text style={topo}>Topo</Text>
+      <Text style={conteudo}>Conteúdo</Text>
+      <Text style={rodape}>Rodapé</Text>
     </View>
   );
 };
