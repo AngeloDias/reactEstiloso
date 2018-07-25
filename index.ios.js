b4bc93426cab87dd104460a116874799e6760340
angelossdias <D14sAng>
2018-07-25 13:54:30 -0300

    Definindo imagem

--- index.ios.js
+++ index.ios.js
@@ -1,38 +1,18 @@
 import React from 'react';
-import {Text, AppRegistry, View, TouchableOpacity} from 'react-native';
+import {Text, AppRegistry, View, Image} from 'react-native';
 
 const Estilos = {
   principal: {
     paddingTop: 40
-  },
-
-  botao: {
-    backgroundColor: '#48BBEC',
-    padding: 10,
-    borderColor: '#1d8eb8',
-    borderWidth: 3,
-    borderRadius: 8,
-    shadowColor: '#000',
-    shadowOffset: {width: 0, height: 2},
-    shadowOpacity: 0.4
-  },
-
-  textoBotao: {
-    color: '#fff',
-    fontSize: 16,
-    fontWeight: 'bold',
-    alignSelf: 'center'
   }
 };
 
 const App = () => {
-  const {principal, botao, textoBotao} = Estilos;
+  const {principal} = Estilos;
 
   return (
     <View style={principal}>
-      <TouchableOpacity style={botao}>
-        <Text style={textoBotao}>Clique aqui</Text>
-      </TouchableOpacity>
+      <Image source={require('./imgs/uvas.png')} />
     </View>
   );
 
